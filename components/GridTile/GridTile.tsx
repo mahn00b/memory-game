399e4360f02112843133cb4c1ba872ee1ee9b294
Mahmoud <myousif123@gmail.com>
2021-10-17 17:29:55 -0400

    Added unit tests for the GridTile.

--- components/GridTile/GridTile.tsx
+++ components/GridTile/GridTile.tsx
@@ -5,17 +5,19 @@ export interface GridTileProps {
   /** Label to attach to button for screen reader support. */
   ariaLabel: string;
   /** Accepts children to display it's value */
-  children: React.ReactNode;
+  children?: React.ReactNode;
   /** Disables pointer events for this tile.
    * @default false
   */
-     disabled?: boolean
+  disabled?: boolean
   /** Accepts a callback function which fires on click */
   onClick: () => void;
   /** The tile's state
    * @default 'hidden'
   */
   state?: 'hidden' | 'active' | 'inactive';
+  /** A string passed into the data-testid property of the parent element. */
+  testId?: string
 }
 
 const GridTile: React.FC<GridTileProps> = ({
@@ -24,12 +26,15 @@ const GridTile: React.FC<GridTileProps> = ({
   disabled,
   onClick,
   state = 'hidden',
+  testId,
 }: GridTileProps) => (
   <button
     aria-label={ariaLabel}
     className={cx(styles.GridTile, styles[state])}
     disabled={disabled}
-    onClick={onClick}>
+    onClick={onClick}
+    data-testid={testId}
+    >
     {children}
   </button>
 )
