2011bcc74f00b3054e1b3b427e5e0f9375eff8c0
Mahmoud <myousif123@gmail.com>
2021-11-21 13:23:09 -0500

    Completed Modal.

---
+++ components/Modal/Modal.test.tsx
@@ -0,0 +1,28 @@
+import { getByText, render } from '@testing-library/react'
+import Modal from './Modal';
+
+describe('Modal', () => {
+  it('should be defined.', () => {
+    expect(Modal).toBeDefined();
+  })
+
+  it('should be closed when isOpen is set to false.', () => {
+    const { queryByTestId, queryByText } = render(
+      <Modal isOpen={false} dataTestid="TEST_ID">
+        <div>test</div>
+      </Modal>
+    )
+
+    expect(queryByTestId('TEST_ID')).not.toHaveClass('isOpen');
+  })
+
+  it('should be open when isOpen is set to true.', () => {
+    const { getByTestId, getByText } = render(
+      <Modal isOpen={true} dataTestid="TEST_ID">
+        <div>test</div>
+      </Modal>
+    )
+
+    expect(getByTestId('TEST_ID')).toHaveClass('isOpen');
+  })
+})
---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,29 @@
+import cx from 'classnames';
+import styles from './Modal.module.scss';
+
+export interface ModalProps {
+  /** Toggles the appearance of the Modal. */
+  isOpen: boolean
+  /** Contents of the Modal are rendered as children */
+  children?: React.ReactNode
+  /** Used to target the Modal in testing environments. */
+  dataTestid?: string
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  children,
+  dataTestid
+}: ModalProps) => {
+  return (
+    <div className={cx(styles.Modal, { [styles.isOpen]: isOpen })} data-testid={dataTestid}>
+      <div className={styles.content}>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+Modal.displayName = 'Modal';
+
+export default Modal;
