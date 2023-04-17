e49b94067f1b112926665f4b230f9c15e9ad7fcd
Mahmoud Yousif <myousif123@gmail.com>
2023-04-17 11:10:44 -0400

    Removed unused prop from the Modal component.

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -6,8 +6,6 @@ export interface ModalProps {
   isOpen: boolean;
   /** Contents of the Modal are rendered as children */
   children?: React.ReactNode;
-  /** Any custom classes that get appended to the modal's in Container. */
-  containerStyles?: string;
   /** Used to target the Modal in testing environments. */
   dataTestid?: string;
 }
@@ -15,11 +13,10 @@ export interface ModalProps {
 const Modal: React.FC<ModalProps> = ({
   isOpen,
   children,
-  containerStyles = '',
   dataTestid
 }: ModalProps) => {
   return (
-    <div className={cx(styles.Modal, { [styles.isOpen]: isOpen }, containerStyles)} data-testid={dataTestid}>
+    <div className={cx(styles.Modal, { [styles.isOpen]: isOpen })} data-testid={dataTestid}>
       <div className={styles.content}>
         {children}
       </div>
